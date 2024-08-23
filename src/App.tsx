9f461bc440b193a961e3d4446250889b449fa754
Arthur Duarte <tuiduarte@gmail.com>
2024-08-22 23:00:55 -0400

    learn: add hooks to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Navbar from './components/Navbar';
 import ListGroup from './components/ListGroup';
 
 export default function App() {
+  let items = ["Paper", "Rock", "Scissors"]
+  const handleSelectItem = (index: number) => {
+    let machineItem = Math.floor(Math.random() * 2)
+    console.log(`You chose ${items[index]} and the machine chose ${items[machineItem]}`)
+
+    if ((index === 0 && machineItem === 1) || (index === 1 && machineItem === 2) || (index === 2 && machineItem === 0)) {
+      console.log("You won")
+    } else if ((index === 1 && machineItem === 0) || (index === 2 && machineItem === 1) || (index === 0 && machineItem === 2)) {
+      console.log("Machine won")
+    } else {
+      console.log("Tie. No winners.")
+    }
+
+  }
+
   return (
     <>
-      <Navbar />
-      <ListGroup />
+      <ListGroup items={items} heading="Options" onSelectItem={handleSelectItem} />
     </>
   )
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,27 @@
-export default function ListGroup(){
-    return <h1>List Group</h1>
+import { useState } from "react"
+
+interface Props {
+    items: string[]
+    heading: string
+    onSelectItem: (index: number) => void
+}
+
+export default function ListGroup({ items, heading, onSelectItem }: Props) {
+    let [selectedOption, setSelectedOption] = useState(-1)
+
+    return (
+        <>
+            <p>{heading}</p>
+            {items.length === 0 && <p>No options found.</p>}
+            {selectedOption != -1 ? <p>You chose {selectedOption} </p> : <p>You didn't choose anything</p>}
+            <ul>
+                {items.map((item, index) => (
+                    <li key={item} onClick={() => {
+                        setSelectedOption(index)
+                        onSelectItem(index)
+                    }} >{item}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
