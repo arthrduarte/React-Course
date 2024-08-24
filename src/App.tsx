e2e697f78c739e743ddbbdde46bcd624f4a2f6b7
Arthur Duarte <tuiduarte@gmail.com>
2024-08-24 17:24:14 -0400

    refactor: separate App.tsx into Header and Game components

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,15 @@
 import { useState } from 'react';
-import Option from './components/Game';
+import Game from './components/Game';
 import Header from './components/Header';
 
 export default function App() {
-  let options = ["Rock", "Paper", "Scissors"]
   let [userScore, setUserScore] = useState(0)
   let [machineScore, setMachineScore] = useState(0)
-  let [winner, setWinner] = useState("Make your first move")
-
-
-  const machineChoice = () => {
-    return options[Math.floor(Math.random() * 2)]
-  }
-
-  const handleSelectItem = (userOption: string) => {
-    let machineOption = machineChoice()
-    console.log(`You chose ${userOption} and the machine chose ${machineOption}`)
-
-    if (
-      (userOption === "Rock" && machineOption === "Scissors") ||
-      (userOption === "Scissors" && machineOption === "Paper") ||
-      (userOption === "Paper" && machineOption === "Rock")) {
-
-      setUserScore((prevScore) => {
-        const newScore = prevScore + 1;
-        setWinner("User won")
-        console.log(`You won. Your score is ${newScore}`)
-        return newScore
-      })
-
-    } else if (
-      (userOption === "Scissors" && machineOption === "Rock") ||
-      (userOption === "Paper" && machineOption === "Scissors") ||
-      (userOption === "Rock" && machineOption === "Paper")) {
-
-      setMachineScore((prevScore) => {
-        const newScore = prevScore + 1;
-        setWinner("Machine won")
-        console.log(`Machine won. Its score is ${machineScore}`)
-        return newScore
-      })
-
-    } else {
-      setWinner("Select your move and the machine will select automaticaly")
-      console.log("Tie. No winners.")
-    }
-
-  }
 
   return (
     <>
       <Header userScore={userScore} machineScore={machineScore} />
-      <div className='flex justify-center p-5'>
-        <div className='flex text-center flex-col w-1/3'>
-          <Option option={options[0]} onSelectOption={handleSelectItem} />
-          <Option option={options[1]} onSelectOption={handleSelectItem} />
-          <Option option={options[2]} onSelectOption={handleSelectItem} />
-        </div>
-        <div className='text-center w-1/3'>
-          <p>{winner}</p>
-        </div>
-        <div className='text-center w-1/3'>
-          <Option option="?" onSelectOption={handleSelectItem} />
-        </div>
-      </div>
+      <Game setUserScore={setUserScore} setMachineScore={setMachineScore} />
     </>
   )
 }
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,15 +1,76 @@
+import { useState } from "react";
+
 interface Props {
-    option: string;
-    onSelectOption: (option: string) => void
+    setUserScore: React.Dispatch<React.SetStateAction<number>>;
+    setMachineScore: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default function Game({ option, onSelectOption }: Props) {
+export default function Game({ setMachineScore, setUserScore }: Props) {
+    let options = ["👊🏼", "🖐🏼", "✌🏼"]
+    let [winner, setWinner] = useState("Make your first move")
+
+    const machineChoice = () => {
+        return options[Math.floor(Math.random() * 2)]
+    }
+
+    const handleSelectItem = (userOption: string) => {
+        let machineOption = machineChoice()
+
+        if (
+            (userOption === options[0] && machineOption === options[2]) ||
+            (userOption === options[2] && machineOption === options[1]) ||
+            (userOption === options[1] && machineOption === options[0])) {
+
+            setUserScore((prevScore) => {
+                const newScore = prevScore + 1;
+                setWinner("User won")
+                return newScore
+            })
+
+        } else if (
+            (userOption === options[2] && machineOption === options[0]) ||
+            (userOption === options[1] && machineOption === options[2]) ||
+            (userOption === options[0] && machineOption === options[1])) {
+
+            setMachineScore((prevScore) => {
+                const newScore = prevScore + 1;
+                setWinner("Machine won")
+                return newScore
+            })
+
+        } else if (userOption === machineOption) {
+            setWinner("Tie. No winners.")
+        } else {
+            setWinner("Select your move and the machine will select automaticaly")
+        }
+
+    }
+
+
+    // Figure out how to add a class to the last selected option. The last clicked option will have a different bg-color
+
     return (
         <>
-            <div className="p-5 border rounded">
-                <p onClick={() => {
-                    onSelectOption(option)
-                }}>{option}</p>
+            <div className='flex justify-center p-5'>
+                <div className='flex flex-col items-center w-1/3 space-y-5'>
+                    <div className="cursor-pointer w-[10rem] h-[10rem] p-5 hover:bg-slate-50 border rounded flex justify-center items-center" onClick={() => handleSelectItem(options[0])}>
+                        <p className=" text-[3rem]">{options[0]}</p>
+                    </div>
+                    <div className="cursor-pointer w-[10rem] h-[10rem] p-5 hover:bg-slate-50 border rounded flex justify-center items-center" onClick={() => handleSelectItem(options[1])}>
+                        <p className=" text-[3rem]">{options[1]}</p>
+                    </div>
+                    <div className="cursor-pointer w-[10rem] h-[10rem] p-5 hover:bg-slate-50 border rounded flex justify-center items-center" onClick={() => handleSelectItem(options[2])}>
+                        <p className=" text-[3rem]">{options[2]}</p>
+                    </div>
+                </div>
+                <div className='text-center w-1/3'>
+                    <p>{winner}</p>
+                </div>
+                <div className='flex flex-col items-center w-1/3'>
+                    <div className="cursor-pointer w-[10rem] h-[10rem] p-5 hover:bg-slate-50 border rounded flex justify-center items-center">
+                        <p className=" text-[3rem]">?</p>
+                    </div>
+                </div>
             </div>
         </>
     )
