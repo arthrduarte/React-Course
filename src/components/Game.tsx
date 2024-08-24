326f2a397db7dc6c41205e5e49100e96b45c91ae
Arthur Duarte <tuiduarte@gmail.com>
2024-08-24 19:52:56 -0400

    feat: add rules Modal

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Modal from "./Modal"
 
 interface Props {
     setUserScore: React.Dispatch<React.SetStateAction<number>>;
@@ -7,10 +8,11 @@ interface Props {
 
 export default function Game({ setMachineScore, setUserScore }: Props) {
     let options = ["👊🏼", "🖐🏼", "✌🏼"]
-    let [winner, setWinner] = useState("Make your move")
-    let [machineSquare, setMachineSquare] = useState("?")
-    let [selectedOption, setSelectedOption] = useState<string | null>(null)
-    let [allowedPlay, setAllowedPlay] = useState(true)
+    const [winner, setWinner] = useState("Make your move")
+    const [machineSquare, setMachineSquare] = useState("?")
+    const [selectedOption, setSelectedOption] = useState<string | null>(null)
+    const [allowedPlay, setAllowedPlay] = useState<boolean>(true)
+    const [open, setOpen] = useState<boolean>(false)
 
     const machineChoice = () => {
         return options[Math.floor(Math.random() * 2)]
@@ -61,29 +63,44 @@ export default function Game({ setMachineScore, setUserScore }: Props) {
         setMachineSquare("?")
     }
 
-
-    // Figure out how to add a class to the last selected option. The last clicked option will have a different bg-color
-
     return (
         <>
             <div className='flex flex-col lg:flex-row justify-center p-5'>
-                <div className='flex lg:flex-col lg:items-center justify-center lg:w-1/5 lg:space-y-5'>
-                    <div className={`cursor-pointer lg:w-[10rem] lg:h-[10rem] w-[7rem] h-[7rem] p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ${selectedOption && selectedOption !== options[0] ? 'hidden' : 'flex'} justify-center items-center`} onClick={() => handleSelectItem(options[0])}>
+                <div className='flex lg:flex-col lg:items-center lg:justify-start justify-center lg:w-1/5'>
+                    <div className={`cursor-pointer lg:mb-5 lg:w-[10rem] lg:h-[10rem] w-[7rem] h-[7rem] p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ${selectedOption && selectedOption !== options[0] ? 'hidden' : 'flex'} justify-center items-center`} onClick={() => handleSelectItem(options[0])}>
                         <p className=" text-[3rem]">{options[0]}</p>
                     </div>
-                    <div className={`cursor-pointer lg:w-[10rem] lg:h-[10rem] w-[7rem] h-[7rem] p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ${selectedOption && selectedOption !== options[1] ? 'hidden' : 'flex'} justify-center items-center`} onClick={() => handleSelectItem(options[1])}>
+                    <div className={`cursor-pointer lg:mb-5 lg:w-[10rem] lg:h-[10rem] w-[7rem] h-[7rem] p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ${selectedOption && selectedOption !== options[1] ? 'hidden' : 'flex'} justify-center items-center`} onClick={() => handleSelectItem(options[1])}>
                         <p className=" text-[3rem]">{options[1]}</p>
                     </div>
-                    <div className={`cursor-pointer lg:w-[10rem] lg:h-[10rem] w-[7rem] h-[7rem] p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ${selectedOption && selectedOption !== options[2] ? 'hidden' : 'flex'} justify-center items-center`} onClick={() => handleSelectItem(options[2])}>
+                    <div className={`cursor-pointer lg:mb-5 lg:w-[10rem] lg:h-[10rem] w-[7rem] h-[7rem] p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ${selectedOption && selectedOption !== options[2] ? 'hidden' : 'flex'} justify-center items-center`} onClick={() => handleSelectItem(options[2])}>
                         <p className=" text-[3rem]">{options[2]}</p>
                     </div>
                 </div>
-                <div className='text-center lg:w-1/5'>
+                <div className='flex flex-col items-center lg:w-1/5'>
                     <p className="font-bold text-[2rem] my-5">{winner}</p>
                     <button
                         onClick={() => handlePlayAgain()}
-                        className="text-white font-bold rounded-lg px-3 py-2 mb-5 transition ease-in bg-[#E84855] hover:bg-[#ff7480]"
+                        className="w-1/2 text-white font-bold rounded-lg px-3 py-2 mb-5 transition ease-in bg-[#E84855] hover:bg-[#ff7480]"
                     >Play Again</button>
+                    <button
+                        onClick={() => setOpen(true)}
+                        className="w-1/2 text-white font-bold rounded-lg px-3 py-2 mb-5 transition ease-in bg-[#0E6BA8] hover:bg-[#72b9e8]">Rules</button>
+                    <Modal open={open} onClose={() => setOpen(false)}>
+                        <div className="flex flex-col gap-4">
+                            <h1 className="text-2xl">The Basics of the Game</h1>
+                            <p>Despite its underlying complexity, the game's rules are straightforward. Players deliver hand signals representing rock, paper, or scissors, with the outcome determined by these three rules:</p>
+                            <ol>
+                                <li>Rock wins against scissors.</li>
+                                <li>Scissors win against paper.</li>
+                                <li>Paper wins against rock.</li>
+                            </ol>
+                            <hr className="border-t-sold border-1 border-grey" />
+                            <div className="flex flex-row justify-center">
+                                <button className="border border-neutral-300 rounded-lg py-2 px-3 bg-[#0E6BA8] hover:bg-[#72b9e8] text-white" onClick={() => setOpen(false)}>Close</button>
+                            </div>
+                        </div>
+                    </Modal>
                 </div>
                 <div className='flex flex-col items-center lg:w-1/5'>
                     <div className="lg:w-[10rem] lg:h-[10rem] w-[7rem] h-[7rem] p-5 bg-white border border-gray-200 rounded-lg shadow flex justify-center items-center">
